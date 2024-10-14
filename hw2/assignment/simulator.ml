b4b71ac9670a1343d4f0971f76dd6dfbbb41d1f7
TheHummel <jannisalsbach@gmail.com>
2024-10-14 21:38:19 +0200

    update simulator

--- hw2/assignment/simulator.ml
+++ hw2/assignment/simulator.ml
@@ -157,11 +157,55 @@ let interp_cnd {fo; fs; fz} : cnd -> bool =
 (* Maps an X86lite address into Some OCaml array index,
    or None if the address is not within the legal address space. *)
 let map_addr (addr:quad) : int option =
-  if Int64.compare addr mem_bot >= 0 && Int64.compare addr mem_top <= 0 then
+  if Int64.compare addr mem_bot >= 0 && Int64.compare addr mem_top < 0 then
     Some (Int64.to_int addr - Int64.to_int mem_bot)
   else
     None
 
+
+
+
+type opnd_val =
+  | Value of int64
+  | MemLoc of int64
+
+let interp_opnd (op: operand) (m: mach) : opnd_val =
+  match op with
+  | Imm (Lit v) -> Value v
+  | Imm (Lbl _) -> failwith "Label not supported"
+  | Reg reg -> Value (Array.get m.regs (rind reg))
+  | Ind1 (Lit imm) -> MemLoc imm
+  | Ind1 (Lbl _) -> failwith "Label not supported"
+  | Ind2 reg -> 
+    let addr = Array.get m.regs (rind reg) in
+    MemLoc addr
+  | Ind3 (Lit disp, reg) -> 
+    let base = Array.get m.regs (rind reg) in
+    MemLoc (Int64.add disp base)
+  | Ind3 (Lbl _, _) -> failwith "Label not supported"
+
+
+let get_mem_val (mem: mem) (addr: int64) : int64 =
+  if addr < mem_bot || Int64.add addr 7L > mem_top then failwith "out of bounds";
+  let addr_int = Int64.to_int addr in
+  let rec read_bytes n acc shift =
+    if n = 8 then acc
+    else match mem.(addr_int + n) with
+      | Byte b -> read_bytes (n + 1) (Int64.logor acc (Int64.shift_left (Int64.of_int (Char.code b)) shift)) (shift + 8)
+      | _ -> raise X86lite_segfault
+  in
+  read_bytes 0 0L 0
+
+
+let set_mem_val (mem: mem) (addr: int64) (value: int64) : unit =
+  if addr < mem_bot || Int64.add addr 7L > mem_top then failwith "out of bounds";
+  let addr_int = Int64.to_int addr in
+  for i = 0 to 7 do
+    let byte = Char.chr (Int64.to_int (Int64.shift_right value (i * 8)) land 0xFF) in
+    match mem.(addr_int + i) with
+    | Byte _ -> mem.(addr_int + i) <- Byte byte
+    | _ -> raise X86lite_segfault
+  done
 
 (* Simulates one step of the machine:
     - fetch the instruction at %rip
@@ -171,7 +215,266 @@ let map_addr (addr:quad) : int option =
     - set the condition flags
 *)
 let step (m:mach) : unit =
-failwith "step unimplemented"
+  let rip = Array.get m.regs (rind Rip) in
+  let instruction = Array.get m.mem (Int64.to_int rip) in
+  begin match instruction with
+  | InsB0 ins ->
+    let (opcode, operands) = ins in
+    begin match opcode with
+    | Incq | Decq | Negq | Notq -> 
+      begin match operands with
+      | op1::[] -> 
+        let dest_val =
+          begin match interp_opnd op1 m with
+          | Value v -> v
+          | MemLoc addr -> get_mem_val m.mem addr
+          end
+        in
+        let new_val =
+          begin match opcode with
+          | Incq -> Int64.succ dest_val
+          | Decq -> Int64.pred dest_val
+          | Negq -> Int64.neg dest_val
+          | Notq -> Int64.lognot dest_val
+          end
+        in
+        begin match interp_opnd op1 m with
+        | Value _ -> 
+          let reg_idx = rind (match op1 with Reg r -> r | _ -> failwith "no register") in
+          Array.set m.regs reg_idx new_val
+        | MemLoc addr ->
+          set_mem_val m.mem addr new_val
+        end
+      | _ -> failwith ""
+      end
+    | Addq | Subq | Imulq | Xorq | Orq | Andq -> 
+      begin match operands with
+      | [op1; op2] ->
+        let src_val =
+          begin match interp_opnd op1 m with
+          | Value v -> v
+          | MemLoc addr -> get_mem_val m.mem addr
+          end
+        in
+        let dest_val =
+          begin match interp_opnd op2 m with
+          | Value v -> v
+          | MemLoc addr -> get_mem_val m.mem addr
+          end
+        in
+        let new_val = 
+          begin match opcode with
+          | Addq -> Int64.add src_val dest_val
+          | Subq -> Int64.sub src_val dest_val
+          | Imulq -> Int64.mul src_val dest_val
+          | Xorq -> Int64.logxor src_val dest_val
+          | Orq -> Int64.logor src_val dest_val
+          | Andq -> Int64.logand src_val dest_val
+          end
+        in
+        begin match interp_opnd op2 m with
+        | Value _ -> 
+          let reg_idx = rind (match op2 with Reg r -> r | _ -> failwith "no register") in
+          Array.set m.regs reg_idx new_val
+        | MemLoc addr ->
+          set_mem_val m.mem addr new_val
+        end
+      | _ -> ()
+      end
+    | Leaq ->
+      begin match operands with
+      | [op1; op2] -> 
+        let addr =
+          match interp_opnd op1 m with
+          | MemLoc addr -> addr
+          | Value _ -> failwith "Leaq source should be an address"
+        in
+        begin match op2 with
+        | Reg reg ->
+          let reg_idx = rind reg in
+          Array.set m.regs reg_idx addr
+        | _ -> failwith "Leaq dest should be a register"
+        end
+      | _ -> ()
+      end
+    | Shlq | Sarq | Shrq -> 
+      begin match operands with
+      | [op1; op2] ->
+        let amt =
+          begin match interp_opnd op1 m with
+          | Value v -> v
+          | MemLoc addr -> get_mem_val m.mem addr
+          end
+        in
+        let dest_val =
+          begin match interp_opnd op2 m with
+          | Value v -> v
+          | MemLoc addr -> get_mem_val m.mem addr
+          end
+        in
+        let new_val = 
+          match opcode with
+          | Shlq -> Int64.shift_left dest_val (Int64.to_int amt)
+          | Sarq -> Int64.shift_right dest_val (Int64.to_int amt)
+          | Shrq -> Int64.shift_right_logical dest_val (Int64.to_int amt)
+        in
+        begin match interp_opnd op2 m with
+        | Value _ -> 
+          let reg_idx = rind (match op2 with Reg r -> r | _ -> failwith "no register") in
+          Array.set m.regs reg_idx new_val
+        | MemLoc addr ->
+          set_mem_val m.mem addr new_val
+        end
+      | _ -> ()
+      end
+    | Movq | Pushq | Popq ->
+      begin match opcode with
+      | Movq ->
+        begin match operands with
+        | [op1; op2] ->
+          let src_val =
+            begin match interp_opnd op1 m with
+            | Value v -> v
+            | MemLoc addr -> get_mem_val m.mem addr
+            end
+          in
+          begin match interp_opnd op2 m with
+          | Value _ -> 
+            let reg_idx = rind (match op2 with Reg r -> r | _ -> failwith "no register") in
+            Array.set m.regs reg_idx src_val
+          | MemLoc addr ->
+            set_mem_val m.mem addr src_val
+          end
+        | _ -> ()
+        end
+      | Pushq ->
+        begin match operands with
+        | [op1] ->
+          let src_val =
+            begin match interp_opnd op1 m with
+            | Value v -> v
+            | MemLoc addr -> get_mem_val m.mem addr
+            end
+          in
+          let rsp_index = rind Rsp in
+          let rsp_val = Array.get m.regs rsp_index in
+          let new_rsp = Int64.sub rsp_val 8L in
+          Array.set m.regs rsp_index new_rsp;
+
+          set_mem_val m.mem new_rsp src_val
+        | _ -> ()
+        end
+      | Popq ->
+        begin match operands with
+        | [op1] ->
+          let rsp_index = rind Rsp in
+          let rsp_val = Array.get m.regs rsp_index in
+          let mem_rsp = get_mem_val m.mem rsp_val in
+
+          let new_rsp = Int64.add rsp_val 8L in
+          Array.set m.regs rsp_index new_rsp;
+
+          begin match op1 with
+          | Reg reg ->
+            let reg_idx = rind reg in
+            Array.set m.regs reg_idx mem_rsp
+          | _ -> failwith "dest must be a register"
+          end
+        end
+      end
+    | Jmp ->
+      begin match operands with
+      | [op1] ->
+        let src_val =
+          begin match interp_opnd op1 m with
+          | Value v -> v
+          | MemLoc addr -> get_mem_val m.mem addr
+          end
+        in
+        let rip_index = rind Rip in
+        Array.set m.regs rip_index src_val
+
+      end
+          
+    | J cnd ->
+      begin match operands with
+      | [op1] ->
+        let src_val =
+          begin match interp_opnd op1 m with
+          | Value v -> v
+          | MemLoc addr -> get_mem_val m.mem addr
+          end
+        in
+        let rip_index = rind Rip in
+
+        if interp_cnd m.flags cnd then
+          Array.set m.regs rip_index src_val
+        else () (* Todo *)
+      end
+
+    | Cmpq ->
+      begin match opcode with
+      | Cmpq ->
+        match operands with
+        | [op1; op2] ->
+          let src_val1 =
+            begin match interp_opnd op1 m with
+            | Value v -> v
+            | MemLoc addr -> get_mem_val m.mem addr
+            end
+          in
+          let src_val2 =
+            begin match interp_opnd op2 m with
+            | Value v -> v
+            | MemLoc addr -> get_mem_val m.mem addr
+            end
+          in
+          m.flags.fs <- src_val1 < src_val2;
+          m.flags.fz <- src_val1 = src_val2;
+          (* Todo *)
+
+        | _ -> ()
+        
+      (* | Set cnd *)
+      | _ -> ()
+        end
+    | Callq | Retq ->
+      let rip_idx = rind Rip in
+      let rsp_idx = rind Rsp in
+      let rip_val = Array.get m.regs rip_idx in
+      let rsp_val = Array.get m.regs rsp_idx in
+
+      match opcode with
+      | Callq ->
+        begin match operands with
+        | [op1] -> 
+          let new_rsp = Int64.sub rsp_val 8L in
+          Array.set m.regs rsp_idx new_rsp;
+    
+          set_mem_val m.mem new_rsp rip_val;
+    
+          let new_rip =
+            match interp_opnd op1 m with
+            | Value addr -> addr
+            | MemLoc addr -> get_mem_val m.mem addr
+          in
+          Array.set m.regs rip_idx new_rip;
+        | _ -> ()
+        end
+
+      | Retq ->
+        let return_addr = get_mem_val m.mem rsp_val in
+        let new_rsp = Int64.add rsp_val 8L in
+        Array.set m.regs rsp_idx new_rsp;
+        Array.set m.regs rip_idx return_addr;
+
+
+    end
+
+  | InsFrag -> ()
+  | Byte char -> ()
+  
+  end
 
 (* Runs the machine until the rip register reaches a designated
    memory address. Returns the contents of %rax when the 
